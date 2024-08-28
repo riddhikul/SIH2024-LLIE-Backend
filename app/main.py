9b09b5d5e79d4de02f53a8b4a984f6981051edad
AdityaMore7000 <adi.more7000@gmail.com>
2024-08-28 15:24:53 +0530

    upload download works!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 from app.api.endpoints import image_processing
-
+import sys 
+import os
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+sys.path.append(os.path.abspath("./app/services"))
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,  # Allows cookies to be sent across domains
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get("/")
 def read_root():
--- app/services/image_enhancement.py
+++ app/services/image_enhancement.py
@@ -2,8 +2,8 @@ import cv2
 
 def reduce_noise(image):
     return cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
-
-def enhance_low_light_image(input_image, clip_limit=2.0, tile_size=(8, 8)):
+def enhance_low_light_image(image,clip_limit=2.0, tile_size=(8, 8)):
+    input_image=reduce_noise(image)
     lab = cv2.cvtColor(input_image, cv2.COLOR_RGB2LAB)
     l, a, b = cv2.split(lab)
     clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_size)
